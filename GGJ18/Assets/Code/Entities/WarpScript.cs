4aba4787c6577f74a7919c7c31cdb6eaaadb7249
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2018-01-27 04:36:58 -0300

    Warps funcionando, se agrego un player para testearlos de funcionalidad simple

---
+++ GGJ18/Assets/Code/Entities/WarpScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpScript : MonoBehaviour {
+
+    public WarpScript WrpDestination;
+    public Transform RecieverPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponentInParent<PlayerWarp>().Warp(WrpDestination.RecieverPoint.position);
+        }
+    }
+}
---
+++ GGJ18/Assets/Code/keyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class keyboardInput : MonoBehaviour {
+
+    private NavMeshAgent meshAgent;
+    public float MoveSpeed;
+
+	void Start () {
+        if (meshAgent == null)
+            meshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) {
+            meshAgent.Move(Vector3.left * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            meshAgent.Move(Vector3.back * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            meshAgent.Move(Vector3.right * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            meshAgent.Move(Vector3.forward * MoveSpeed);
+        }
+    }
+}
