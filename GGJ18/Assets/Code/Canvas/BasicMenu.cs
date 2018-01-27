487445a8f4e6fdc9f6d37e1808cdc58556c96993
Juancho <juan.rod707@gmail.com>
2018-01-27 19:10:20 -0300

    Integracion de arte placeholder

---
+++ GGJ18/Assets/Code/Canvas/BasicMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BasicMenu : MonoBehaviour
+{
+    public void NavigateToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
