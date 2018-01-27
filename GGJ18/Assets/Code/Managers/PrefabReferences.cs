3c93246d46b29a364aad3350b0cb40062c85fb67
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2018-01-27 15:29:42 -0300

    SpawnPoints + Tweaks

--- GGJ18/Assets/Code/Managers/PrefabReferences.cs
+++ GGJ18/Assets/Code/Managers/PrefabReferences.cs
@@ -7,9 +7,14 @@ using UnityEngine;
 public class PrefabReferences : MonoBehaviour
 {
     public PrefabIdPair[] Minions;
+    public PrefabIdPair[] Npcs;
 
     public GameObject GetMinionById(string id)
     {
         return Minions.First(x => x.PfId.Equals(id)).Prefab;
+    }
+    public GameObject GetNpcById(string id)
+    {
+        return Npcs.First(x => x.PfId.Equals(id)).Prefab;
     }
 }
