e0c001311be049f422ec1c18affda510da3826cd
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2018-01-28 14:27:00 -0300

    Melee button press working, Special, only robot is working.

---
+++ GGJ18/Assets/Code/Canvas/ButtonPressEfect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonPressEfect : MonoBehaviour {
+
+	public float HighLigthTimer;
+	public Color HighLigth;
+	private Color defaultColor;
+	private Image myImage;
+
+	void Start(){
+		myImage = this.GetComponent<Image>();
+		defaultColor = myImage.color;
+	}
+
+	public void PressButton(){
+		myImage.color = HighLigth;
+		StartCoroutine(WaitHightligth());
+	}
+
+	private IEnumerator WaitHightligth(){
+		yield return new WaitForSeconds(HighLigthTimer);
+		ResetButtonColor();
+	}
+	private void ResetButtonColor(){
+		myImage.color = defaultColor;
+	}
+}
