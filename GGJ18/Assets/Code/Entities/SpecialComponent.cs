cea93f68f4f61ab8ae4bab9e55b62e0eecdc4331
Marccio Silva <msilva@tavanoteam.com>
2018-01-28 14:09:56 -0300

    Arreglos en efecto de particulas de shockwave (material en vez de frame).

--- GGJ18/Assets/Code/Entities/SpecialComponent.cs
+++ GGJ18/Assets/Code/Entities/SpecialComponent.cs
@@ -1,12 +1,18 @@
-﻿using Assets.Code.Helpers;
+﻿using System;
+using Assets.Code.Helpers;
 using UnityEngine;
 
 public class SpecialComponent : MonoBehaviour
 {
-    public GameObject robotAreaDamageEffectPrefab;
-    public float SecondsBetweenSpecials = 2.0f;
+    public GameObject areaDamageEffectPrefab;
+    public float SecondsBetweenSpecials = 4.0f;
     private float timeSinceLastSpecial;
     public Faction Faction;
+
+    private void Start()
+    {
+        timeSinceLastSpecial = SecondsBetweenSpecials;
+    }
 
     private void Update()
     {
@@ -16,13 +22,10 @@ public class SpecialComponent : MonoBehaviour
 
     public void AttemptSpecial()
     {
-        if (timeSinceLastSpecial > SecondsBetweenSpecials)
+        if (timeSinceLastSpecial >= SecondsBetweenSpecials)
         {
             timeSinceLastSpecial = 0.0f;
-            if (Faction == Faction.Robot)
-            {
-                Instantiate(robotAreaDamageEffectPrefab, this.transform.position, Quaternion.identity);
-            }
+            Instantiate(areaDamageEffectPrefab, this.transform.position, Quaternion.identity);
         }
     }
 }
