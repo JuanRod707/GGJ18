a2172c72c816a1a44db21f955c2dadeddf6f6fe2
Marccio Silva <msilva@tavanoteam.com>
2018-01-28 11:01:54 -0300

    Fix en area damage effect.

--- GGJ18/Assets/Code/Entities/AreaDamageEffect.cs
+++ GGJ18/Assets/Code/Entities/AreaDamageEffect.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class AreaDamageEffect : MonoBehaviour
 {
     public List<GameObject> EntitiesInsideTrigger;
-    public float SecondsBeforeEffectStarts = 2;
+    public float SecondsBeforeEffectStarts = 1;
+    public AudioSource SpecialSfx;
 
     private void Start()
     {
         EntitiesInsideTrigger = new List<GameObject>();
+        StartCoroutine(ActivateEffect());
     }
 
-    public void ActivateEffect()
+    public IEnumerator ActivateEffect()
     {
-//        yield return new WaitForSeconds(SecondsBeforeEffectStarts);
+        yield return new WaitForSeconds(SecondsBeforeEffectStarts);
+        SpecialSfx.Play();
         EntitiesInsideTrigger.RemoveAll(x => x == null);
         foreach (var entity in EntitiesInsideTrigger)
         {
-            if (!entity.CompareTag(Constants.tagPlayer))
+            if (!entity.CompareTag(Constants.tagPlayer) &&
+                !entity.transform.parent.CompareTag(Constants.tagPlayer))
             {
+                Debug.Log(string.Format("Tag is: {0}", entity.tag));
                 // TODO handle npc and minion cases.
                 Destroy(entity);
             }
--- GGJ18/Assets/Code/Entities/SpecialComponent.cs
+++ GGJ18/Assets/Code/Entities/SpecialComponent.cs
@@ -2,33 +2,22 @@
 
 public class SpecialComponent : MonoBehaviour
 {
-    private AreaDamageEffect areaDamageEffect;
+    public GameObject areaDamageEffect;
     public float SecondsBetweenSpecials = 2.0f;
     private float timeSinceLastSpecial;
-    public AudioSource SpecialSfx;
-
-    void Start()
-    {
-        areaDamageEffect = GetComponentInChildren<AreaDamageEffect>();
-    }
 
     private void Update()
     {
         timeSinceLastSpecial += Time.deltaTime;
-        Debug.Log(string.Format("Time since last special is {0}", timeSinceLastSpecial));
+//        Debug.Log(string.Format("Time since last special is {0}", timeSinceLastSpecial));
     }
 
     public void AttemptSpecial()
     {
-        if (areaDamageEffect != null)
+        if (timeSinceLastSpecial > SecondsBetweenSpecials)
         {
-            if (timeSinceLastSpecial > SecondsBetweenSpecials)
-            {
-                timeSinceLastSpecial = 0.0f;
-
-                areaDamageEffect.ActivateEffect();
-                SpecialSfx.Play();
-            }
+            timeSinceLastSpecial = 0.0f;
+            Instantiate(areaDamageEffect, this.transform.position, Quaternion.identity);
         }
     }
 }
